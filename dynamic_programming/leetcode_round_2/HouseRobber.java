a880865b4f859c0f18c46cc7c050fed750817d1b
Zhi Li <lizhieffe@gmail.com>
2016-08-10 17:08:35 -0700

    3

--- dynamic_programming/leetcode_round_2/HouseRobber.java
+++
@@ -1,5 +0,0 @@
-package leetcode_round_2;
-
-public class HouseRobber {
-
-}
---
+++ dynamic_programming/leetcode_round_2/HouserRobber.java
@@ -0,0 +1,19 @@
+package leetcode_round_2;
+
+public class HouserRobber {
+	public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int rob = nums[0];
+        int norob = 0;
+        
+        for(int i = 1; i < nums.length; i++) {
+            int currrob = norob + nums[i];
+            int currnorob = Math.max(norob, rob);
+            rob = currrob;
+            norob = currnorob;
+        }
+        return Math.max(rob, norob);
+    }
+	
+// https://discuss.leetcode.com/topic/12024/java-dp-solution-o-n-runtime-and-o-1-space-with-inline-comment
+}
