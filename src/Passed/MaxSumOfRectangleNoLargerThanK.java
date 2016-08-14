d7e44e721e700591e41bd95880585ce29547fcc8
Zhi Li <lizhieffe@gmail.com>
2016-08-14 16:38:58 -0700

    3

---
+++ src/Passed/MaxSumOfRectangleNoLargerThanK.java
@@ -0,0 +1,46 @@
+package Passed;
+
+public class MaxSumOfRectangleNoLargerThanK {
+	private int[][] sum;
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        if (matrix == null || matrix.length == 0) return 0;
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        calcSum(matrix);
+        int max = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < m; i++) {
+            for(int p = i; p < m; p++) {
+                for(int j = 0; j < n; j++) {
+                    for(int q = j; q < n; q++) {
+                        int rsum = sumRegion(i,j,p,q);
+                        if (rsum <= k) {
+                            max = Math.max(max, rsum);
+                        }
+                    }
+                }
+            }
+        }
+        return max;
+    }
+    
+    void calcSum(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) return;
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        sum = new int[m+1][n+1];
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + matrix[i-1][j-1];
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sum[row2+1][col2+1] - sum[row2+1][col1] - sum[row1][col2+1] + sum[row1][col1];
+    }
+	
+}
